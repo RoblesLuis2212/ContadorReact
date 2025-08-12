00315d23e24eb0035e09403fbf8ed85d36f5b3a0
Luis Robles <luisgeremiasrobles@gmail.com>
2025-08-12 20:06:12 -0300

    Se agrego el componente TeoriaComponente

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Bienvenidos from "./components/Bienvenidos"
+import TeoriaComponente from "./components/TeoriaComponente"
 
 function App() {
   //Aqui va la logica del componente
   return (
     //Aqui se puede agregar mas logica
-    <>
+    <main className="container my-5">
     <h1>Hola Mundo!</h1>
     {/* <Bienvenidos></Bienvenidos> */}
     <Bienvenidos/>
-    </>
+    <TeoriaComponente/>
+    </main>
   )
 }
 
